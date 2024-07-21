0835771f792ce89829dab2e28a2184327a797e44
BorgovsDouble3321 <borgovsdouble@gmail.com>
2024-07-21 22:04:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, render_template_string
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.proxy import Proxy, ProxyType
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
@@ -21,29 +16,19 @@ def home():
         </form>
     '''
 
+
 @app.route('/scrape', methods=['POST'])
 def scrape():
     jeopardy_id = request.form['game_id']
     url = f'https://jeopardylabs.com/play/{jeopardy_id}'
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    response = requests.get(url)
+    response.raise_for_status()
 
-    service = Service(ChromeDriverManager().install())
-
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get(url)
-
-    driver.find_element(By.ID, "submit").click()
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
     back_questions = soup.find_all(class_='back question')
 
     answers = [q.get_text(strip=True) for q in back_questions]
-
-    driver.quit()
 
     num_columns = 5
     columns = [[] for _ in range(num_columns)]
@@ -64,6 +49,7 @@ def scrape():
         </div>
         <a href="/">Zurück zur Startseite</a>
     ''', columns=columns)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
